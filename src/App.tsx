2b419ee7b215073845cfb1d22e373f9e5b04f1e0
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2023-05-29 18:20:24 -0300

    fix: removing search and feed from the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,10 @@
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import GlobalTheme from './styles/theme';
-import { SearchBar } from './components/Base/SearchBar';
-import { CardFeed } from './components/CardFeed';
-
-import image01 from './assets/foto01.png';
-import image02 from './assets/foto02.png';
-import image03 from './assets/foto03.png';
-import { FeedContainer } from './components/FeedContainer/FeedContainer';
 
 export function App() {
   return (
     <ThemeProvider theme={GlobalTheme}>
-      <div style={{ paddingTop: '250px' }}>
-        <SearchBar />
-      </div>
-      <FeedContainer>
-        <CardFeed
-          cardSize="small"
-          title="Baxter"
-          description="French Bulldoog, 5-year-old, 12kgs"
-          background={image01}
-        />
-        <CardFeed
-          cardSize="large"
-          title="Baxter"
-          description="French Bulldoog, 5-year-old, 12kgs"
-          background={image02}
-        />
-        <CardFeed
-          cardSize="large"
-          title="Baxter"
-          description="French Bulldoog, 5-year-old, 12kgs"
-          background={image01}
-        />
-        <CardFeed
-          cardSize="small"
-          title="Baxter"
-          description="French Bulldoog, 5-year-old, 12kgs"
-          background={image02}
-        />
-        <CardFeed
-          cardSize="small"
-          title="Baxter"
-          description="French Bulldoog, 5-year-old, 12kgs"
-          background={image03}
-        />
-        <CardFeed
-          cardSize="small"
-          title="Baxter"
-          description="French Bulldoog, 5-year-old, 12kgs"
-          background={image01}
-        />
-        <CardFeed
-          cardSize="small"
-          title="Baxter"
-          description="French Bulldoog, 5-year-old, 12kgs"
-          background={image03}
-        />
-      </FeedContainer>
       <GlobalStyle />
     </ThemeProvider>
   );
